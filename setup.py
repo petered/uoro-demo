f382c0d0266be64eacaef956ed44e9feed1972ea
Peter O'Connor <peter.ed.oconnor@gmail.com>
2018-01-05 16:41:52 -0800

    added initial files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "uoro-demo",
+    author = "peter",
+    version = 0,
+    dependency_links = (),
+    install_requires = ['numpy', 'matplotlib', 'pytorch'],
+    scripts = [],
+    packages=find_packages(),
+    )
